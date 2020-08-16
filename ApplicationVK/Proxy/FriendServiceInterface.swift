c1cef207ce94729acd1e5103f61b3ee34acdf816
Oksana_Zvereva <zvereva.oksana@gmail.com>
2020-08-16 22:18:09 +0500

    proxy

---
+++ ApplicationVK/Proxy/FriendServiceInterface.swift
@@ -0,0 +1,9 @@
+//
+//  FriendServiceInterface.swift
+//  ApplicationVK
+//
+//  Created by Оксана Зверева on 16.08.2020.
+//  Copyright © 2020 Oksana Zvereva. All rights reserved.
+//
+
+import Foundation
---
+++ ApplicationVK/Proxy/FriendServiceProxy.swift
@@ -0,0 +1,9 @@
+//
+//  FriendServiceProxy.swift
+//  ApplicationVK
+//
+//  Created by Оксана Зверева on 16.08.2020.
+//  Copyright © 2020 Oksana Zvereva. All rights reserved.
+//
+
+import Foundation
